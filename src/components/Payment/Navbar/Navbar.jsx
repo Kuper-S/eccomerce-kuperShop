f7a68ab994a0fdf9e2f08fc35b69329b642374f4
Kuper-S <kupkup1992@gmail.com>
2022-09-21 08:23:22 +0300

    initial navbar

--- src/components/Payment/Navbar/Navbar.jsx
+++ src/components/Payment/Navbar/Navbar.jsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import styled from "styled-components"
+import SearchIcon from '@mui/icons-material/Search';
 const Container = styled.div`
     height: 60px;
-    background-color: black;
+    
 `
 
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content : space-around;
+
+`
+
+const Left = styled.div`
+    flex :1;
+`
+
+const Languages = styled.span`
+  font-size : 14px;
+  cursor = pointer;   
+`
+const Center = styled.div`
+    flex :1;
+`
+const Right = styled.div`
+    flex :1;
+`
+
+const SearchContainer = styled.div`
+
+`
 
 const Navbar = () => {
   return (
     <Container>
-      navbar
-      </Container>
+        <Wrapper>
+            <Left>
+                <Languages>
+                    EN
+                </Languages>
+                <SearchContainer>
+                    input
+                    <SearchIcon/>
+                </SearchContainer>
+            </Left>
+            <Center>Center</Center>
+            <Right>Right</Right>
+        </Wrapper>
+    </Container>
   )
 }
 
